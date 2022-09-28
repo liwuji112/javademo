6f3ec4cd72f38fb973ed975227fa9895a7988dd5
jackli <1172140738@qq.com>
2022-09-28 11:37:39 +0800

    first day

---
+++ day01/tb_user.sql
@@ -0,0 +1,42 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 80017
+Source Host           : localhost:3306
+Source Database       : loginuser
+
+Target Server Type    : MYSQL
+Target Server Version : 80017
+File Encoding         : 65001
+
+Date: 2022-09-28 11:28:55
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `avatar` varchar(256) DEFAULT NULL COMMENT '头像',
+  `user_name` varchar(64) NOT NULL COMMENT '用户名',
+  `password` varchar(16) NOT NULL COMMENT '用户密码',
+  `email` varchar(128) NOT NULL COMMENT '邮箱',
+  `create_time` INTEGER unsigned DEFAULT NULL COMMENT '创建时间',
+  `update_time` INTEGER unsigned DEFAULT NULL COMMENT '更新时间',
+  `last_login_time` INTEGER unsigned DEFAULT NULL COMMENT '最后登陆时间',
+  `is_deleted` tinyint(2) DEFAULT NULL COMMENT '是否删除',
+  `status` tinyint(2) DEFAULT NULL COMMENT '是否禁用状态标识符',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `email` (`email`),
+  KEY `u_idx_user_name` (`user_name`),
+  KEY `u_idx_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
+
+-- ----------------------------
+-- Records of tb_user
+-- ----------------------------
